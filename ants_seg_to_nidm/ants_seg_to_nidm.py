428db6164987fe504051c341083389478db922dd
Yibei Chen <yibeichan@gmail.com>
2025-12-02 11:51:58 -0500

    rm unused lines

--- ants_seg_to_nidm/ants_seg_to_nidm.py
+++ ants_seg_to_nidm/ants_seg_to_nidm.py
@@ -228,8 +228,6 @@ def add_seg_data(nidmdoc,subjid,stats_entity_id, add_to_nidm=False, forceagent=F
             """
             PREFIX prov: <http://www.w3.org/ns/prov#>
             PREFIX nidm: <http://purl.org/nidash/nidm#>
-            PREFIX sio: <http://semanticscience.org/resource/>
-            PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
             SELECT DISTINCT ?acqObj
             WHERE {
                 ?acqObj a nidm:AcquisitionObject ;
